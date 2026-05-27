40189238e8d4599459f43b9177e3af47df9fdd93
Michael Stringer <stringbean@users.noreply.github.com>
2026-05-27 12:23:42 +0100

    Copy templates

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,9 +1,10 @@
 import commonjs from '@rollup/plugin-commonjs';
+import copy from 'rollup-plugin-copy';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
 
-const config = {
+export default {
   input: 'src/index.ts',
   output: {
     esModule: true,
@@ -11,7 +12,14 @@ const config = {
     format: 'es',
     sourcemap: true,
   },
-  plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs(), json()],
+  plugins: [
+    copy({
+      targets: [{ src: 'src/templates', dest: 'dist' }],
+    }),
+    typescript(),
+    nodeResolve({ preferBuiltins: true }),
+    commonjs(),
+    // needed as some dependencies load JSON
+    json(),
+  ],
 };
-
-export default config;
